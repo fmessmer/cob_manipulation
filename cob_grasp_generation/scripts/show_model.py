508a3553b26973ecf7753d50de42019b7ade4911
ipa-fxm <felix.messmer@ipa.fhg.de>
2016-04-09 15:55:05 +0200

    add sdh solo env model and show script

---
+++ cob_grasp_generation/scripts/show_model.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+import os
+import rospy
+import roslib
+from cob_grasp_generation import or_grasp_generation
+
+if __name__ == '__main__':
+  rospy.init_node('show_model')
+  os.environ['OPENRAVE_DATA'] = roslib.packages.get_pkg_dir('cob_grasp_generation')+'/files' # required to find local OR-Data
+  orgg = or_grasp_generation.ORGraspGeneration()
+  #orgg.setup_environment("pringles", "cob3", True)
+  orgg.setup_environment("pringles", "sdh", True)
+  rospy.spin()
